7d9a012c7bdc7b3591394bea44cc332e03202c3f
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-01-09 20:43:26 -0800

    Last Fix

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -3,13 +3,14 @@ class Dev < ActiveRecord::Base
   has_many :companies, through: :freebies
 
   def received_one?(item_name)
-    found = self.freebies.find_by(item_name: item_name)
-    found ? true : false
+    self.freebies.any? { |f| f.item_name == item_name }
   end
 
   def give_away(dev, freebie)
-    if self.freebies.select {|f| f.item_name == freebie.item_name}
-      freebie.dev = dev
+    if self.freebies.select do |f| 
+      if(f.item_name == freebie.item_name)
+        freebie.dev = dev
+      end
     end
   end
 end
